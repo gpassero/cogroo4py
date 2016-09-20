0672f507c164866577632fd250cca7f67052a68d
Guilherme Passero <guilherme.passer@gmail.com>
2016-09-20 00:49:11 -0300

    versão com instalador (setup.py)

--- cogroo_interface.py
+++ cogroo_interface.py
@@ -4,13 +4,54 @@ Created on Sat Sep  3 21:21:19 2016
 
 @author: Guilherme Passero <guilherme.passero0@gmail.com>
 """
-
 from py4j.java_gateway import JavaGateway
-from singleton import Singleton
-from commons import find
 from functools import lru_cache
 import re
 
+class Singleton:
+    """
+    A non-thread-safe helper class to ease implementing singletons.
+    This should be used as a decorator -- not a metaclass -- to the
+    class that should be a singleton.
+
+    The decorated class can define one `__init__` function that
+    takes only the `self` argument. Other than that, there are
+    no restrictions that apply to the decorated class.
+
+    To get the singleton instance, use the `Instance` method. Trying
+    to use `__call__` will result in a `TypeError` being raised.
+
+    Limitations: The decorated class cannot be inherited from.
+
+    """
+
+    def __init__(self, decorated):
+        self._decorated = decorated
+
+    def Instance(self):
+        """
+        Returns the singleton instance. Upon its first call, it creates a
+        new instance of the decorated class and calls its `__init__` method.
+        On all subsequent calls, the already created instance is returned.
+
+        """
+        try:
+            return self._instance
+        except AttributeError:
+            self._instance = self._decorated()
+            return self._instance
+
+    def __call__(self):
+        raise TypeError('Singletons must be accessed through `Instance()`.')
+
+    def __instancecheck__(self, inst):
+        return isinstance(inst, self._decorated)
+
+		
+def find(pattern, text):
+    return [(i.start(), i.end()) for i in re.finditer(pattern, text)]
+
+	
 class Token:
     def __init__(self, cogroo_token):
         self.lemmas = list(cogroo_token.getLemmas())
---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='cogroo_interface',
+      version='0.1',
+      py_modules=['cogroo_interface'],
+      )
