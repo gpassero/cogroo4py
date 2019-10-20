977e3308903c5b9490f6665ba94b17a2c4e27bcc
Guilherme Passero <guilherme.passer@gmail.com>
2019-10-20 10:15:50 -0300

    update version 0.2 -> 0.3

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 setup(name='cogroo_interface',
-      version='0.2',      
-	  description='Interface for accessing CoGrOO from Python scripts using py4j',
+      version='0.3',      
+	  description='An interface to use CoGrOO (a grammar checker for the Portuguese language) with Python using py4j',
 	  author='Guilherme Passero',
 	  author_email='guilherme.passero0@gmail.com',
 	  url='https://github.com/gpassero/cogroo4py',
-	  py_modules=['cogroo_interface']
+	  py_modules=['cogroo_interface'],
+	  install_requires=['py4j'],
 )
